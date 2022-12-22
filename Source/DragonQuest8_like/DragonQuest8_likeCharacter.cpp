176b2fecbef20a1e4b5b045783be89b908860d99
matsubara-kanta <45473854+matsubara-kanta@users.noreply.github.com>
2022-12-23 01:39:23 +0900

    バトルシーンにキャラを配置

--- Source/DragonQuest8_like/DragonQuest8_likeCharacter.cpp
+++ Source/DragonQuest8_like/DragonQuest8_likeCharacter.cpp
@@ -75,8 +75,8 @@ void ADragonQuest8_likeCharacter::SetupPlayerInputComponent(class UInputComponen
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
 		
 		//Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
 		//Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ADragonQuest8_likeCharacter::Move);
